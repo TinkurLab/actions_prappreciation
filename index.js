ec00a947e4158f389c19837812b408dccf564258
Adam Zolyak <adam.zolyak@gmail.com>
2018-10-24 17:21:44 -0600

    here goes

--- index.js
+++ index.js
@@ -2,28 +2,33 @@ console.log("started nodejs...")
 
 const octokit = require('@octokit/rest')()
 
-console.log("GITHUB_ACTION")
-console.log(process.env.GITHUB_ACTION)
-console.log()
+const ownerAndRepo = process.env.GITHUB_REPOSITORY	
+const slicePos = ownerAndRepo.indexOf("/");
+const owner = ownerAndRepo.slice(0, slicePos);
+const repo = ownerAndRepo.slice(slicePos + 1, ownerAndRepo.length);
 
-console.log("GITHUB_SHA")
-console.log(process.env.GITHUB_SHA)
-console.log()
-
-console.log("GITHUB_REF")
-console.log(process.env.GITHUB_REF)
-console.log()
-
-console.log("GITHUB_EVENT_PATH")
-console.log(process.env.GITHUB_EVENT_PATH)
-console.log()
-
-const fs = require('fs');
+const fs = require('fs')
 const filePath = '../github/workflow/event.json';
 fs.readFile(filePath, "utf8", function(error, data) {
     if (error) {
       console.error("read error:  " + error.message);
     } else {
-      console.log(data);
+        updatePRTitle(data)
     }
 });
+
+function updatePRTitle(event) {
+
+    console.log(event.title)
+    console.log(event.number)
+
+    const prNumber = event.number
+
+    octokit.pullRequests.update({
+        owner: owner,
+        repo: repo,
+        number: prNumber
+    }).then(({ data, headers, status }) => {
+        console.log(data)
+    })
+}

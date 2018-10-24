eb3f99cd1119417554b0618b95511603335c5c34
Adam Zolyak <adam.zolyak@gmail.com>
2018-10-24 15:55:12 -0600

    take 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,15 @@
 
 echo "starting bash..."
 
+cat event.json
+
+export GITHUB_EVENT_PAYLOAD=$(cat event.json)
+
+echo "contents of GITHUB_EVENT_PAYLOAD is " GITHUB_EVENT_PAYLOAD
+
 echo "current working directory is " $PWD
 cd ../../
 cd /action
-
-export GITHUB_EVENT_PAYLOAD=$(cat /github/workflow/event.json)
-
-echo "contents of GITHUB_EVENT_PAYLOAD is " GITHUB_EVENT_PAYLOAD
 
 npm install
 

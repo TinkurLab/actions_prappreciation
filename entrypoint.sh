91d92940d7bb441a0a0d12e509995c56bf23a251
Adam Zolyak <adam.zolyak@gmail.com>
2018-10-24 15:14:04 -0600

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,20 @@ echo "starting bash..."
 
 echo "current working directory is " $PWD
 
+ls
+
+cd ..
+
+echo "current working directory is " $PWD
+
+ls
+
+cd ..
+
+echo "current working directory is " $PWD
+
+ls
+
 npm install
 
 node index.js

1353f40cf1e80189b7f97a12428700af8b056e6e
Adam Zolyak <adam.zolyak@gmail.com>
2018-10-24 18:26:44 -0600

    for realz realz

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ octokit.authenticate({
     token: process.env.GITHUB_TOKEN
 })
 
-//const eventOwnerAndRepo = process.env.GITHUB_REPOSITORY	
-const eventOwnerAndRepo = "adamzolyak/actions-helloworld"
+const eventOwnerAndRepo = process.env.GITHUB_REPOSITORY	
 const slicePos = eventOwnerAndRepo.indexOf("/");
 const eventOwner = eventOwnerAndRepo.slice(0, slicePos);
 const eventRepo = eventOwnerAndRepo.slice(slicePos + 1, eventOwnerAndRepo.length);
@@ -36,13 +35,11 @@ async function updatePRTitle() {
     eventData = await readFilePromise('../github/workflow/event.json')
     eventJSON = JSON.parse(eventData) 
 
-    //const eventPRNumber = eventJSON.number
-    const eventPRNumber = "2"
+    const eventPRNumber = eventJSON.number
 
     console.log("PR Title: " + eventJSON.pull_request.title)
     console.log("PR Number: " + eventPRNumber)
 
-    /*
     octokit.pullRequests.update({
         owner: eventOwner,
         repo: eventRepo,
@@ -51,15 +48,6 @@ async function updatePRTitle() {
     }).then(({ data, headers, status }) => {
         console.log(data)
     })
-    */
-
-   octokit.pullRequests.get({
-    owner: eventOwner,
-    repo: eventRepo,
-    number: eventPRNumber
-}).then(({ data, headers, status }) => {
-    console.log(data)
-})
 }
 
 updatePRTitle()
